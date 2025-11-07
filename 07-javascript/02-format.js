e7352ffcf17978aa586ce6130160510993c621c9
astave <astave@pdx.edu>
2025-11-07 10:31:52 -0800

    Week 6 submission

--- 07-javascript/02-format.js
+++ 07-javascript/02-format.js
@@ -1,10 +1,15 @@
 const formatItems = (input) => {
   // return a string of the concatenated numbers, separated by commas and with three digits after the decimal point
+  let result = [];
+
+  for (let item of input) {
+    result.push(item.toFixed(3));
+  }
+
+  return result.join(", ");
 
   // for reference, see the MDN Number page:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
-
-  return input;
 };
 
 console.log(formatItems([1, 4, 17, 26, 41]));
--- 07-javascript/04-dom.js
+++ 07-javascript/04-dom.js
@@ -2,16 +2,33 @@
 // when the user clicks the 'copy' button, copy the user input to the output area
 
 // fetch JavaScript objects representing specific elements in the DOM
+let inputElement = document.getElementById("userInput1");
+let copy = document.querySelector("#copy");
+let output = document.querySelector(".output");
 
 // add an event listener on the target element
+copy.addEventListener("click", handleClick);
 
 // callback function to handle event
+function handleClick(event) {
+  output.textContent = inputElement.value;
+}
 
 // Exercise #2:
 // when the user enters input text, copy the user input to the output area
 
 // fetch JavaScript objects representing specific elements in the DOM
+let inputElement2 = document.getElementById("userInput2");
 
 // add an event listener on the target element
+inputElement2.addEventListener("input", handleInput);
+
+let section = document.querySelector("#inputEventExample");
+let element = document.createElement("div");
+element.setAttribute("class", "output");
+section.append(element);
 
 // callback function to handle event
+function handleInput(event) {
+  element.textContent = inputElement2.value;
+}
