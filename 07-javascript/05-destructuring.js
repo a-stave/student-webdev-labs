8f030bf6002ade46691ad545910566fb59845b9d
astave <astave@pdx.edu>
2025-11-20 18:23:59 -0800

    Week 8 Submission

--- 07-javascript/05-destructuring.js
+++ 07-javascript/05-destructuring.js
@@ -1,35 +1,38 @@
 const destructureItems = (input) => {
   // destructure the name and parents of the character and format the output as shown
+  const name = `${input.first} ${input.last}`;
+  const { mother, father } = input.allegiance.parents;
 
-  return input;
+  const output = `${name}\n  daughter of\n  ${mother} and ${father}`;
+  return output;
 };
 
 const sansa = {
-  first: 'Sansa',
-  last: 'Stark',
+  first: "Sansa",
+  last: "Stark",
   allegiance: {
-    house: 'Winterfell',
+    house: "Winterfell",
     parents: {
-      mother: 'Catelyn Tully',
-      father: 'Eddard Stark',
+      mother: "Catelyn Tully",
+      father: "Eddard Stark",
     },
     animals: {
-      direwolf: 'Lady',
+      direwolf: "Lady",
     },
   },
 };
 
 const daenerys = {
-  first: 'Daenerys',
-  last: 'Targaryen',
+  first: "Daenerys",
+  last: "Targaryen",
   allegiance: {
-    house: 'Targaryen',
+    house: "Targaryen",
     parents: {
-      mother: 'Queen Rhaella',
-      father: 'King Aerys II Targaryen',
+      mother: "Queen Rhaella",
+      father: "King Aerys II Targaryen",
     },
     animals: {
-      dragons: 'Drogon',
+      dragons: "Drogon",
     },
   },
 };
--- 07-javascript/06-fetch.js
+++ 07-javascript/06-fetch.js
@@ -1,9 +1,59 @@
-const url = 'https://anapioficeandfire.com/api/books/';
+const url = "https://anapioficeandfire.com/api/books/";
 
-const app = document.querySelector('#books');
+const app = document.querySelector("#books");
+
+const addBookToDOM = (book) => {
+  // Create elements to be added to the DOM
+  let element = document.createElement("div");
+  let title = document.createElement("h3");
+  let author = document.createElement("p");
+  let released = document.createElement("p");
+  let pages = document.createElement("p");
+
+  // Set content
+  title.textContent = book.name;
+  author.textContent = book.authors[0];
+  released.textContent = book.released.substr(0, 4);
+  pages.textContent = `${book.numberOfPages} pages`;
+
+  // Append to DOM
+  element.append(title);
+  element.append(author);
+  element.append(released);
+  element.append(pages);
+
+  // Update styles to center the books
+  element.style.display = "flex";
+  element.style.flexDirection = "column";
+  element.style.alignItems = "center";
+  element.style.marginTop = "20px";
+
+  // Append the book element to the app container
+  app.append(element);
+};
 
 const fetchData = (url) => {
   // Fetch all books from the API of Ice and Fire and append them to the DOM
   // Create an element for each book that contains title, author, publication year, and number of pages
   // Update the styles in JavaScript to center all the books in the container given
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((book) => {
+        addBookToDOM(book);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      let element = document.createElement("div");
+      element.textContent = "An error occurred while fetching the data.";
+      app.append(element);
+    })
+    .finally(() => {
+      let loader = document.querySelector("#loading");
+      app.removeChild(loader);
+    });
 };
+
+fetchData(url);
